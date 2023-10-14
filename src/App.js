b7a9b8bfb2272fbfa970823b07aa13e091f12734
Nalam Guna Sri Krishna <102983513+GunaNalam@users.noreply.github.com>
2023-10-14 21:05:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import Task1 from './components/Task1';
+import Task5 from './components/Task5';
 class App extends Component {
   
   render() {
     return(
       <div className="App">
-        <Task1 />
+        <Task5 />
       </div>
     );
   }
