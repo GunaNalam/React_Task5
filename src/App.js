6cbba2c0c56e6b769b4a4e9aaf1810481745b432
Nalam Guna Sri Krishna <102983513+GunaNalam@users.noreply.github.com>
2023-10-14 20:00:09 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import Task1 from './components/Task1';
+class App extends Component {
+  
+  render() {
+    return(
+      <div className="App">
+        <Task1 />
+      </div>
+    );
+  }
+}
+
+export default App;
+ 
